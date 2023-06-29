9915f95e946abf6612796282a60ea368f0c26219
RobertMarte13 <rproject133gmail.com>
2023-06-29 09:47:41 -0400

    Arreglando variables de entorno

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY)
+export const supabase = createClient('https://tyehfhynxwjuwdnzpryj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5ZWhmaHlueHdqdXdkbnpwcnlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc5NTM1MDEsImV4cCI6MjAwMzUyOTUwMX0.0b3tEoE9U3fjWUTlj9kYuagPpM7meu4E-Vaz2cmo124')
